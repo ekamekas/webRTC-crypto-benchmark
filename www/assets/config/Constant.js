b7bb4e4f007c56604abb6f7d5b737610be0b702a
Mas Eka Setiawan <mas.ekamekas@gmail.com>
2018-04-10 11:43:33 +0700

    Add Constant for configuration

--- www/assets/config/Constant.js
+++ www/assets/config/Constant.js
@@ -3,6 +3,10 @@ const Constant = {
     SDP_TYPE : {
         OFFER : "offer",
         ANSWER : "answer"
+    },
+    SERVER : {
+        HTTP : "http://webrtc.laman.local:8081",
+        HTTPS : "https://webrtc.laman.local:8082"
     }
 
 };
